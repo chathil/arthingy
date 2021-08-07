663403777c87ae8a819f3a90689e85b86c5c5dba
Abdul Chathil <chathil98@gmail.com>
2021-08-07 12:53:21 +0700

    add ios

---
+++ ios/Pods/Target Support Files/Pods-Wikipix/Pods-Wikipix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wikipix : NSObject
+@end
+@implementation PodsDummy_Pods_Wikipix
+@end
---
+++ ios/Wikipix/WikipixApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikipixApp.swift
+//  Wikipix
+//
+//  Created by Abdul Chathil on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WikipixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
